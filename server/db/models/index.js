f48bd9e13848df6243fc704a2ccda1ad26a388d3
Spencer Whitehead <SpencerWhitehead7@gmail.com>
2018-10-23 23:36:21 -0400

    feat: actual user table in DB for account system

--- server/db/models/index.js
+++ server/db/models/index.js
@@ -1,7 +1,9 @@
+const User = require(`./user`)
 const TokenModel = require(`./tokenModel`)
 
 // Make associations here
 
 module.exports = {
+  User,
   TokenModel,
 }
---
+++ server/db/models/user.js
@@ -0,0 +1,56 @@
+const Sequelize = require(`sequelize`)
+const crypto = require(`crypto`)
+
+const db = require(`../database`)
+
+const User = db.define(`user`, {
+  email : {
+    type : Sequelize.STRING,
+    unqiue : true,
+    allowNull : false,
+  },
+  password : {
+    type : Sequelize.STRING,
+    allowNull : false,
+    get(){
+      return () => this.getDataValue(`salt`)
+    },
+  },
+  salt : {
+    type : Sequelize.STRING,
+    get(){
+      return () => this.getDataValue(`salt`)
+    },
+  },
+})
+
+// Instance methods
+User.prototype.correctPassword = function(passwordAttempt){
+  return User.encryptPassword(passwordAttempt, this.salt()) === this.password()
+}
+
+// Class methods
+User.generateSalt = function(){
+  return crypto.randomBytes(16).toString(`base64`)
+}
+
+User.encryptPassword = function(newPasswordPlainText, salt){
+  return crypto
+    .createHash(`RSA-SHA256`)
+    .update(newPasswordPlainText)
+    .update(salt)
+    .digest(`hex`)
+}
+
+const setSaltAndPassword = user => {
+  if(user.changed(`password`)){
+    user.salt = User.generateSalt()
+    user.password = User.encryptPassword(user.password(), user.salt())
+  }
+}
+
+// Hooks
+User.beforeCreate(setSaltAndPassword)
+User.beforeUpdate(setSaltAndPassword)
+
+module.exports = User
